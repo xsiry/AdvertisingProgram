e5316481dc89925e1a960904e0394db638961bb6
xsiry <xsiries@gmail.com>
2017-03-03 16:27:23 +0800

    #图片链接管理构建

---
+++ static/img_link_mgr/src/img_link_mgr_data.js
@@ -0,0 +1,20 @@
+define({
+	Rows: [{
+		"Id": "123123",
+		"Name": "扑鱼达人",
+		"MenuSort": "热门游戏",
+		"Preview": "马化腾",
+		"LatestCommit": "陈二狗",
+		"UpdatedAt": "2016-12-15 16:07",
+		"Apply": true
+	}, {
+		"Id": "323422",
+		"Name": "魔法王座",
+		"MenuSort": "热门游戏",
+		"Preview": "丁磊",
+		"LatestCommit": "李小花",
+		"UpdatedAt": "2016-12-15 16:07",
+		"Apply": false
+	}],
+	Total: 2
+})
