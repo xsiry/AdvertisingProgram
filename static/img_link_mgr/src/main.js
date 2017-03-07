4b0012d7580af7b8ef0e37d51c6ce6a7601526db
xsiry <xsiries@gmail.com>
2017-03-07 10:49:46 +0800

    #seajs cache clean

--- static/img_link_mgr/src/main.js
+++ static/img_link_mgr/src/main.js
@@ -3,4 +3,13 @@ define(function(require) {
   var data = require('./img_link_mgr_data');
   var img_link_mgr = require('./img_link_mgr');
   img_link_mgr.init(data);
+
+    // 清理 缓存
+  var url = require.resolve('./main')
+  delete seajs.cache[url]
+  delete seajs.data.fetchedList[url]
+
+  var url = require.resolve('./img_link_mgr')
+  delete seajs.cache[url]
+  delete seajs.data.fetchedList[url]
 });
--- static/vertising_mgr/src/main.js
+++ static/vertising_mgr/src/main.js
@@ -3,4 +3,13 @@ define(function(require) {
   var data = require('./vertising_mgr_data');
   var vertising_mnt = require('./vertising_mgr');
   vertising_mnt.init(data);
+
+  // 清理 缓存
+  var url = require.resolve('./main')
+  delete seajs.cache[url]
+  delete seajs.data.fetchedList[url]
+
+  var url = require.resolve('./vertising_mgr')
+  delete seajs.cache[url]
+  delete seajs.data.fetchedList[url]
 });
