bdad389fc08d8c4dda2bebfa57c8f9f5cac3a546
xsiry <xsiries@gmail.com>
2017-03-08 10:21:28 +0800

    #菜单分类广告

---
+++ static/level_class_two/src/data.js
@@ -0,0 +1,58 @@
+define({
+  Rows: [{
+    "Id": "1",
+    "Name": "热门大作",
+    "SuperClass": "游戏",
+    "Direction": "链接",
+    "Link": "",
+    "LatestCommit": "陈二狗",
+    "UpdatedAt": "2016-12-15 16:07",
+    "Apply": true
+  }, {
+    "Id": "2",
+    "Name": "赚钱过年",
+    "SuperClass": "游戏",
+    "Direction": "链接",
+    "Link": "http://www.baidu.com",
+    "LatestCommit": "李小花",
+    "UpdatedAt": "2016-12-15 16:07",
+    "Apply": false
+  }, {
+    "Id": "3",
+    "Name": "单机游戏",
+    "SuperClass": "游戏",
+    "Direction": "链接",
+    "Link": "http://www.baidu.com",
+    "LatestCommit": "李小花",
+    "UpdatedAt": "2016-12-15 16:07",
+    "Apply": false
+  }, {
+    "Id": "4",
+    "Name": "捕鱼来了",
+    "SuperClass": "手游",
+    "Direction": "链接",
+    "Link": "http://www.baidu.com",
+    "LatestCommit": "李小花",
+    "UpdatedAt": "2016-12-15 16:07",
+    "Apply": false
+  }, {
+    "Id": "5",
+    "Name": "街头篮球",
+    "SuperClass": "手游",
+    "Direction": "链接",
+    "Link": "http://www.baidu.com",
+    "LatestCommit": "李小花",
+    "UpdatedAt": "2016-12-15 16:07",
+    "Apply": false
+  }, {
+    "Id": "6",
+    "Name": "梦幻诛仙",
+    "SuperClass": "手游",
+    "Direction": "链接",
+    "Link": "http://www.baidu.com",
+    "LatestCommit": "李小花",
+    "UpdatedAt": "2016-12-15 16:07",
+    "Apply": false
+  }],
+  Total: 6
+})
