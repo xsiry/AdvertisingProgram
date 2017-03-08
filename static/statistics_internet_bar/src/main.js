de41f1e7088dbae407252c2646365de5dd4ba66d
xsiry <xsiries@gmail.com>
2017-03-08 09:55:34 +0800

    #网吧统计

---
+++ static/statistics_internet_bar/src/main.js
@@ -0,0 +1,15 @@
+define(function(require) {
+
+  var data = require('./data');
+  var module = require('./module');
+  module.init(data);
+
+  // 清理 缓存
+  var url = require.resolve('./main')
+  delete seajs.cache[url]
+  delete seajs.data.fetchedList[url]
+
+  var url = require.resolve('./icon_mgr')
+  delete seajs.cache[url]
+  delete seajs.data.fetchedList[url]
+});
